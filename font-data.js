4066f310dd4de786845f4f1a539ca5fb1ca0b5b4
Miguel Fondeur <miguelfondeur@gmail.com>
2025-03-06 23:18:46 -0500

    fix functionality add knowledge page

--- font-data.js
+++ font-data.js
@@ -1,9 +1,249 @@
 export const fonts = [
     {
-        name: "Serif", 
-        declaration: "serif",
+        name: "Apple System", 
+        declaration: "-apple-system, BlinkMacSystemFont",
+        weights: [400, 700],
+        type: "sans-serif"
+    },
+    {
+        name: "Arial", 
+        declaration: "Arial",
+        weights: [400, 700],
+        type: "sans-serif"
+    },    
+    {
+        name: "Arial Black", 
+        declaration: "'Arial Black'",
+        weights: [400, 700],
+        type: ""
+    },
+    {
+        name: "Arial Rounded Bold", 
+        declaration: "Arial Rounded MT Bold",
+        weights: [400, 700],
+        type: ""
+    },
+    {
+        name: "Baskerville", 
+        declaration: "Baskerville",
         weights: [400, 700],
         type: "serif"
+    }, 
+    // {
+    //     name: "Baskerville Old Face", 
+    //     declaration: "Baskerville Old Face",
+    //     weights: [400, 700],
+    //     type: "serif"
+    // },  
+    // {
+    //     name: "Book Antiqua", 
+    //     declaration: "Book Antiqua",
+    //     weights: [400, 700],
+    //     type: "serif"
+    // },  
+    {
+        name: "Brush Script MT", 
+        declaration: "Brush Script MT",
+        weights: [400, 700],
+        type: "cursive"
+    },
+    // {
+    //     name: "Calibri", 
+    //     declaration: "Calibri",
+    //     weights: [400, 700],
+    //     type: "sans-serif"
+    // },
+    // {
+    //     name: "Calisto", 
+    //     declaration: "Calisto MT",
+    //     weights: [400, 700],
+    //     type: "serif"
+    // },
+    // {
+    //     name: "Candara", 
+    //     declaration: "Candara",
+    //     weights: [400, 700],
+    //     type: "sans-serif"
+    // },
+    // {
+    //     name: "Century", 
+    //     declaration: "Century Gothic",
+    //     weights: [400, 700],
+    //     type: "sans-serif"
+    // },
+    {
+        name: "Comic Sans", 
+        declaration: "'Comic Sans MS'",
+        weights: [400, 700],
+        type: "sans-serif"
+    },
+    // {
+    //     name: "Consolas", 
+    //     declaration: "Consolas",
+    //     weights: [400, 700],
+    //     type: "monospace"
+    // },
+    {
+        name: "Copperplate", 
+        declaration: "Copperplate",
+        weights: [400, 700],
+        type: "serif"
+    },
+    // {
+    //     name: "Copperplate Gothic Light", 
+    //     declaration: "Copperplate Gothic Light",
+    //     weights: [400, 700],
+    //     type: "serif"
+    // },
+    {
+        name: "Courier New", 
+        declaration: "Courier",
+        weights: [400, 700],
+        type: "monospace"
+    },
+    {
+        name: "Cursive", 
+        declaration: "Cursive",
+        weights: [400, 700],
+        type: "cursive"
+    },
+    // {
+    //     name: "Franklin Gothic", 
+    //     declaration: "Franklin Gothic Medium",
+    //     weights: [400, 700],
+    //     type: "sans-serif"
+    // },
+    // {
+    //     name: "Frutiger", 
+    //     declaration: "Frutiger",
+    //     weights: [400, 700],
+    //     type: "sans-serif"
+    // },
+    {
+        name: "Futura", 
+        declaration: "Futura",
+        weights: [400, 700],
+        type: "sans-serif"
+    },
+    // {
+    //     name: "Garamond", 
+    //     declaration: "Garamond",
+    //     weights: [400, 700],
+    //     type: "serif"
+    // },  
+    {
+        name: "Geneva", 
+        declaration: "Geneva",
+        weights: [400, 700],
+        type: "sans-serif"
+    },
+    {
+        name: "Georgia", 
+        declaration: "Georgia",
+        weights: [400, 700],
+        type: "serif"
+    },
+    // {
+    //     name: "Goudy Old Style", 
+    //     declaration: "Goudy Old Style",
+    //     weights: [400, 700],
+    //     type: "serif"
+    // },
+    {
+        name: "Helvetica", 
+        declaration: "Helvetica",
+        weights: [400, 700],
+        type: "sans-serif"
+    },
+    {
+        name: "Helvetica Neue", 
+        declaration: "Helvetica Neue",
+        weights: [400, 700],
+        type: "sans-serif"
+    },
+    {
+        name: "Hoefler Text", 
+        declaration: "Hoefler Text",
+        weights: [400, 700],
+        type: "serif"
+    },  
+    {
+        name: "Impact", 
+        declaration: "Impact",
+        weights: [400, 700],
+        type: "sans-serif"
+    },
+    // {
+    //     name: "Lucida", 
+    //     declaration: "Lucida Sans Unicode",
+    //     weights: [400, 700],
+    //     type: "sans-serif"
+    // },
+    // {
+    //     name: "Lucida Console", 
+    //     declaration: "Lucida Console",
+    //     weights: [400, 700],
+    //     type: "monospace"
+    // },
+    {
+        name: "Monaco", 
+        declaration: "monaco",
+        weights: [400, 700],
+        type: "monospace"
+    },
+    {
+        name: "Optima", 
+        declaration: "Optima",
+        weights: [400, 700],
+        type: "sans-serif"
+    },
+    {
+        name: "Palatino", 
+        declaration: "'Palatino'",
+        weights: [400, 700],
+        type: "serif"
+    },  
+    {
+        name: "Palatino Linotype", 
+        declaration: "'Palatino Linotype'",
+        weights: [400, 700],
+        type: "serif"
+    },    
+    {
+        name: "Papyrus", 
+        declaration: "Papyrus",
+        weights: [400, 700],
+        type: "fantasy"
+    },
+    {
+        name: "Rockwell", 
+        declaration: "Rockwell",
+        weights: [400, 700],
+        type: "serif"
+    },  
+    {
+        name: "Sans Serif", 
+        declaration: "sans-serif",
+        weights: [400, 700],
+        type: "sans-serif"
+    },       
+    // {
+    //     name: "Serif", 
+    //     declaration: "serif",
+    //     weights: [400, 700],
+    //     type: "serif"
+    // },
+    // {
+    //     name: "Segoe UI", 
+    //     declaration: "Segoe UI",
+    //     weights: [400, 700],
+    //     type: "sans-serif"
+    // },
+    {
+        name: "Tahoma", 
+        declaration: "Tahoma",
+        weights: [400, 700],
+        type: "sans-serif"
     },
     {
         name: "Times", 
@@ -18,237 +258,15 @@ export const fonts = [
         type: "serif"
     },
     {
-        name: "Georgia", 
-        declaration: "Georgia",
-        weights: [400, 700],
-        type: "serif"
-    },
-    {
-        name: "Garamond", 
-        declaration: "Garamond",
-        weights: [400, 700],
-        type: "serif"
-    },  
-    {
-        name: "Palatino", 
-        declaration: "'Palatino'",
-        weights: [400, 700],
-        type: "serif"
-    },  
-    {
-        name: "Palatino Linotype", 
-        declaration: "'Palatino Linotype'",
-        weights: [400, 700],
-        type: "serif"
-    },    
-    {
-        name: "Book Antiqua", 
-        declaration: "Book Antiqua",
-        weights: [400, 700],
-        type: "serif"
-    },    
-    {
-        name: "Goudy", 
-        declaration: "fantasy",
-        weights: [400, 700],
-        type: "serif"
-    },   
-    {
-        name: "Baskerville", 
-        declaration: "Baskerville",
-        weights: [400, 700],
-        type: "serif"
-    }, 
-    {
-        name: "Baskerville Old Face", 
-        declaration: "Baskerville Old Face",
-        weights: [400, 700],
-        type: "serif"
-    },  
-    {
-        name: "Hoefler Text", 
-        declaration: "Hoefler Text",
-        weights: [400, 700],
-        type: "serif"
-    },  
-    {
-        name: "Rockwell", 
-        declaration: "fantasy",
-        weights: [400, 700],
-        type: "serif"
-    },    
-    {
-        name: "Calisto", 
-        declaration: "fantasy",
-        weights: [400, 700],
-         type: "serif"
-    }, 
-    {
-        name: "Sans Serif", 
-        declaration: "fantasy",
-        weights: [400, 700],
-        type: "sans serif"
-    },       
-    {
-        name: "Arial", 
-        declaration: "fantasy",
-        weights: [400, 700],
-        type: "sans serif"
-    },    
-    {
-        name: "Arial Rounded Bold", 
-        declaration: "fantasy",
-        weights: [400, 700],
-        type: ""
-    },
-    {
-        name: "Arial Black", 
-        declaration: "fantasy",
-        weights: [400, 700],
-        type: ""
-    },
-    {
-        name: "Lucida", 
-        declaration: "fantasy",
-        weights: [400, 700],
-        type: "sans serif"
-    },    
-    {
-        name: "Tahoma", 
-        declaration: "fantasy",
-        weights: [400, 700],
-        type: "sans serif"
-    },
-    {
         name: "Trebuchet MS", 
-        declaration: "fantasy",
+        declaration: "'Trebuchet MS'",
         weights: [400, 700],
-        type: "sans serif"
+        type: "sans-serif"
     },    
     {
         name: "Verdana", 
-        declaration: "fantasy",
+        declaration: "Verdana",
         weights: [400, 700],
-        type: "sans serif"
-    },    
-    {
-        name: "Helvetica", 
-        declaration: "fantasy",
-        weights: [400, 700],
-        type: "sans serif"
-    },
-    {
-        name: "Apple System", 
-        declaration: "fantasy",
-        weights: [400, 700],
-        type: "sans serif"
-    },
-    {
-        name: "Geneva", 
-        declaration: "fantasy",
-        weights: [400, 700],
-        type: "sans serif"
-    },
-    {
-        name: "Franklin Gothic", 
-        declaration: "fantasy",
-        weights: [400, 700],
-        type: "sans serif"
-    },
-    {
-        name: "Futura", 
-        declaration: "fantasy",
-        weights: [400, 700],
-        type: "sans serif"
-    },
-    {
-        name: "Impact", 
-        declaration: "fantasy",
-        weights: [400, 700],
-        type: "sans serif"
-    },
-    {
-        name: "Century", 
-        declaration: "fantasy",
-        weights: [400, 700],
-        type: "sans serif"
-    },
-    {
-        name: "Calibri", 
-        declaration: "fantasy",
-        weights: [400, 700],
-        type: "sans serif"
-    },
-    {
-        name: "Candara", 
-        declaration: "fantasy",
-        weights: [400, 700],
-        type: "sans serif"
-    },
-    {
-        name: "Segoe UI", 
-        declaration: "fantasy",
-        weights: [400, 700],
-        type: "sans serif"
-    },
-    {
-        name: "Optima", 
-        declaration: "fantasy",
-        weights: [400, 700],
-        type: "sans serif"
-    },
-    {
-        name: "Courier New", 
-        declaration: "fantasy",
-        weights: [400, 700],
-        type: "monospace"
-    },
-    {
-        name: "Lucida Console", 
-        declaration: "fantasy",
-        weights: [400, 700],
-        type: "monospace"
-    },
-    {
-        name: "Consolas", 
-        declaration: "fantasy",
-        weights: [400, 700],
-        type: "monospace"
-    },
-    {
-        name: "Monaco", 
-        declaration: "fantasy",
-        weights: [400, 700],
-        type: "monospace"
-    },
-    {
-        name: "Copperplate", 
-        declaration: "fantasy",
-        weights: [400, 700],
-        type: "serif"
-    },
-    {
-        name: "Brush Script MT", 
-        declaration: "fantasy",
-        weights: [400, 700],
-        type: "cursive"
-    },
-    {
-        name: "Cursive", 
-        declaration: "fantasy",
-        weights: [400, 700],
-        type: "cursive"
-    },
-    {
-        name: "Papyrus", 
-        declaration: "fantasy",
-        weights: [400, 700],
-        type: "fantasy"
-    },
-    {
-        name: "Comic Sans", 
-        declaration: "fantasy",
-        weights: [400, 700],
-        type: "sans serif"
-    },
-]
+        type: "sans-serif"
+    }
+];
--- index.js
+++ index.js
@@ -1,23 +1,83 @@
 'use strict'
 
-//Imports
+// Imports
 import { fonts } from './font-data.js';
 
-//Elements
+// Elements
 const cardWrapper = document.getElementById('card-wrapper');
+const filters = document.getElementById('filters');
+const previewInput = document.getElementById('sample-text');
+const searchInput = document.getElementById('search'); // Font search input
+const styleSelects = document.querySelectorAll('.style-select input[type="checkbox"]');
 
-//Loop throught font Data
-fonts.forEach( font => {
-    let cardDisabled = !document.fonts.check(`12px ${font.declaration}`);
+let filterOpen = filters.getAttribute('data-open') === "true";
 
-    const html = `
-        <button class="card" disabled="${cardDisabled}">
-            <div class="card-top">
-                <h3>${ font.name }</h3>
-                <p>${ font.weights.length } Styles</p>
-            </div>
-            <p class="text-example" style="font-family: ${ font.declaration };">The quick brown fox jumps over a lazy dog</p>
-        </button>
-    `;
-    cardWrapper.insertAdjacentHTML('beforeEnd', html )
-})
+// Function to render fonts based on selected filters and search input
+const renderFonts = () => {
+    const selectedTypes = [...styleSelects]
+        .filter(cb => cb.checked)
+        .map(cb => cb.name); // Get checked font styles
+
+    const searchQuery = searchInput.value.trim().toLowerCase(); // Get search input
+
+    // Clear existing font cards
+    cardWrapper.innerHTML = '';
+
+    // Filter fonts based on styles and search query
+    const filteredFonts = fonts.filter(font => {
+        const matchesType = selectedTypes.length ? selectedTypes.includes(font.type) : true;
+        const matchesSearch = font.name.toLowerCase().includes(searchQuery); // Checks if query exists anywhere in the name
+        return matchesType && matchesSearch;
+    });
+
+    // Render filtered fonts
+    filteredFonts.forEach(font => {
+        let cardDisabled = !document.fonts.check(`12px ${font.declaration}`);
+
+        const html = `
+            <button class="card" ${cardDisabled ? 'disabled' : ''}>
+                <div class="card-top">
+                    <h3>${font.name}</h3>
+                    <p>${font.weights.length} Styles</p>
+                </div>
+                <p class="text-example" style="font-family: ${font.declaration};">
+                    The quick brown fox jumps over a lazy dog
+                </p>
+            </button>`;
+
+        cardWrapper.insertAdjacentHTML('beforeEnd', html);
+    });
+};
+
+// Initial render
+renderFonts();
+
+// Click event for toggling filters
+document.addEventListener('click', e => {
+    const filterTrigger = e.target.closest("#filters-button");
+    if (filterTrigger) {
+        filterOpen = !filterOpen;
+        filters.setAttribute('data-open', filterOpen);
+    }
+});
+
+// Update text dynamically in preview
+previewInput.addEventListener('keyup', () => {
+    document.querySelectorAll('.text-example').forEach(element => {
+        element.textContent = previewInput.value || "The quick brown fox jumps over a lazy dog";
+    });
+});
+
+// Handle checkbox changes for filtering & UI state
+styleSelects.forEach(checkbox => {
+    checkbox.addEventListener('change', () => {
+        const label = checkbox.closest('.style-select');
+        label.setAttribute('data-selected', checkbox.checked);
+        renderFonts(); // Re-render fonts on checkbox change
+    });
+});
+
+// Handle search input (filters for any matching part of the name)
+searchInput.addEventListener('keyup', () => {
+    renderFonts(); // Re-render fonts on search input
+});
